55cdaf69a36d4fedaa16277e5d1b3e82140d47e8
AdityaSingh996 <adityasingh8851832459@gmail.com>
2021-06-20 19:28:34 +0530

    added game end

--- sketch.js
+++ sketch.js
@@ -10,7 +10,18 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+function preload(){
 
+ track=loadImage("../images/track.jpg")
+ car1img=loadImage("../images/car1.png")
+ car2img=loadImage("../images/car2.png")
+ car3img=loadImage("../images/car3.png")
+ car4img=loadImage("../images/car4.png")
+ ground=loadImage("../images/ground.png")
+
+
+
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +38,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState===2){
+    game.end();
   }
 }
